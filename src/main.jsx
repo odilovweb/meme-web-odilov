ff2f1ebbebf25ebf0b92e571e9df7c620d2e7846
odilovweb <mirzohidodilov49@gmail.com>
2024-11-05 17:38:12 +0500

    wsd

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <TonConnectUIProvider manifestUrl="https://dashing-phoenix-353291.netlify.app/batfrog-app.json">
+  <TonConnectUIProvider manifestUrl="https://lustrous-hamster-31605a.netlify.app/batfrog-app.json">
     <App />
   </TonConnectUIProvider>
 );
